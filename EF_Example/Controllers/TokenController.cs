c4aa6d26f7d9d12946b7348256d267f42aea79a6
x_coding <l2xguard@live.com>
2022-10-01 01:36:47 +0300

    increase JWT token expire to 1 day

--- EF_Example/Controllers/TokenController.cs
+++ EF_Example/Controllers/TokenController.cs
@@ -56,7 +56,7 @@ namespace EF_Example.Controllers
                         _configuration["Jwt:Issuer"],
                         _configuration["Jwt:Audience"],
                         claims,
-                        expires: DateTime.UtcNow.AddMinutes(10),
+                        expires: DateTime.UtcNow.AddDays(1),
                         signingCredentials: signIn);
 
                     return Ok(new JwtSecurityTokenHandler().WriteToken(token));
