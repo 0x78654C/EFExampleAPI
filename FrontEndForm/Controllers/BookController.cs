5624cfa91fad3daa1cbfae07fe35ba78d80ba125
x_Coding <xcoding.dev@gmail.com>
2023-03-15 23:23:13 +0200

    added class for manage book API data

---
+++ FrontEndForm/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndForm.Controllers
+{
+    public class BookController
+    {
+        private void GetUserData(string apiUrl, string token)
+        {
+            string endpoint = apiUrl;
+
+            WebClient wc = new WebClient();
+            wc.Headers["Content-Type"] = "application/json";
+            wc.Headers["Authorization"] = $"Bearer {token}";
+            try
+            {
+                string response = wc.DownloadString(endpoint);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
