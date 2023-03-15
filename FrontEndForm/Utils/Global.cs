d2748984cded42b43988a74afb9682f223ec2c43
x_Coding <xcoding.dev@gmail.com>
2023-03-15 23:24:56 +0200

    created global variables file

---
+++ FrontEndForm/Utils/Global.cs
@@ -0,0 +1,10 @@
+﻿using System.Security;
+
+namespace FrontEndForm.Utils
+{
+    public class Global
+    {
+        public const string apiUrl = "https://localhost:7142";
+        public static SecureString jwtKey { get; set; }
+    }
+}
