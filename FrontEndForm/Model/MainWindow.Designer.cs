25be4320b0858fef1030e22876a80243f01d7dbd
x_Coding <xcoding.dev@gmail.com>
2023-03-15 23:24:42 +0200

    added new form for book manage after user login

---
+++ FrontEndForm/Model/MainWindow.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FrontEndForm.Model
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            BooksLbl = new Label();
+            bookName = new DataGridViewTextBoxColumn();
+            author = new DataGridViewTextBoxColumn();
+            catergory = new DataGridViewTextBoxColumn();
+            price = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { bookName, author, catergory, price });
+            dataGridView1.Location = new Point(12, 44);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(684, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // BooksLbl
+            // 
+            BooksLbl.AutoSize = true;
+            BooksLbl.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            BooksLbl.Location = new Point(12, 26);
+            BooksLbl.Name = "BooksLbl";
+            BooksLbl.Size = new Size(91, 15);
+            BooksLbl.TabIndex = 1;
+            BooksLbl.Text = "Books in Store:";
+            // 
+            // bookName
+            // 
+            bookName.HeaderText = "Name";
+            bookName.Name = "bookName";
+            bookName.Width = 240;
+            // 
+            // author
+            // 
+            author.HeaderText = "Author";
+            author.Name = "author";
+            author.Width = 150;
+            // 
+            // catergory
+            // 
+            catergory.HeaderText = "Catergory";
+            catergory.Name = "catergory";
+            catergory.Width = 150;
+            // 
+            // price
+            // 
+            price.HeaderText = "Price";
+            price.Name = "price";
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(702, 218);
+            Controls.Add(BooksLbl);
+            Controls.Add(dataGridView1);
+            Name = "MainWindow";
+            Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label BooksLbl;
+        private DataGridViewTextBoxColumn bookName;
+        private DataGridViewTextBoxColumn author;
+        private DataGridViewTextBoxColumn catergory;
+        private DataGridViewTextBoxColumn price;
+    }
+}
