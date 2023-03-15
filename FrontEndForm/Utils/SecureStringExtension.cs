0af7928e85078e83485e15a78dd67597820d1ce9
x_Coding <xcoding.dev@gmail.com>
2023-03-15 23:25:24 +0200

    created secure string extensions used for store jwt key in use

---
+++ FrontEndForm/Utils/SecureStringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Security;
+
+namespace FrontEndForm
+{
+    public static class SecureStringExtension
+    {
+        /// <summary>
+        /// Convert the SecureString to string.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string SecureStringToString(this SecureString data) => new System.Net.NetworkCredential(string.Empty, data).Password;
+
+        /// <summary>
+        /// Convert string to SecureString.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static SecureString StringToSecureString(this string data)
+        {
+            var secureString = new SecureString();
+            foreach(var c in data)
+                secureString.AppendChar(c);
+            return secureString;
+        }
+    }
+}
