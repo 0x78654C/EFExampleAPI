7a5b4bcd8e0f69d04fefb99c28a333839e323e63
x_Coding <xcoding.dev@gmail.com>
2023-03-16 00:02:56 +0200

    created constructor for get apiurl and token params

--- FrontEndForm/Controllers/BookController.cs
+++ FrontEndForm/Controllers/BookController.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Http.Headers;
 
 namespace FrontEndForm.Controllers
 {
     public class BookController
     {
+        private string ApiUrl { get; set; }
+        private string Token { get; set; }
+        public BookController(string apiUrl, string token)
+        {
+            ApiUrl = apiUrl;
+            Token = token;
+        }
+
+        [Obsolete]
         private void GetUserData(string apiUrl, string token)
         {
             string endpoint = apiUrl;
@@ -23,6 +28,34 @@ namespace FrontEndForm.Controllers
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
+            }
+        }
+        public async Task<string> GetData( bool isAuth)
+        {
+            if (string.IsNullOrEmpty(Token))
+            {
+                return "No token provided";
+            }
+
+            HttpClient hc = new HttpClient();
+            hc.DefaultRequestHeaders.Accept.Clear();
+            if (isAuth)  hc.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+            hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                HttpResponseMessage responseMessage = await hc.GetAsync(ApiUrl);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var responseJson = await responseMessage.Content.ReadAsStringAsync();
+                  return responseJson;
+                }
+                else
+                    return await responseMessage.Content.ReadAsStringAsync();
+            }
+            catch (Exception e)
+            {
+                return $"Error: {e.ToString()}";
             }
         }
     }
