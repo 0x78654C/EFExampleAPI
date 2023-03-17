b1b2379f9e2e877e7be43c986661d26d89e7737a
x_Coding <xcoding.dev@gmail.com>
2023-03-17 18:38:59 +0200

    create mode for boos store view

---
+++ FrontEndForm/Model/Books.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEndForm
+{
+    public class Books
+    {
+        public long id { get; set; }
+        public string? Book_Name { get; set; }
+        public string? Author { get; set; }
+        public string? Catergory { get; set; }
+
+        public int Amount { get; set; }
+        public int Price { get; set; }
+    }
+}
