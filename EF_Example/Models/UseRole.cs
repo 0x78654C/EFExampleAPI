09459131c384ee1f66b13d52deda26e7df013471
x_Coding <xcoding.dev@gmail.com>
2023-03-19 08:59:56 +0200

    uppdated userlogin and added userrole model

---
+++ EF_Example/Models/UseRole.cs
@@ -0,0 +1,13 @@
+﻿namespace EF_Example.Models
+{
+    public class UseRole
+    {
+        public long Id { get; set; }
+        public long RoleId { get; set; }
+        public long UserId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public virtual Role? Role { get; set; }
+
+        public virtual UserLogin? User { get; set; }
+    }
+}
