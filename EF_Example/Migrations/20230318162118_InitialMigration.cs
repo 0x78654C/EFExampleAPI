65ece71ee7f647e90b07fb96d949851dc1c7db65
x_Coding <xcoding.dev@gmail.com>
2023-03-19 08:58:58 +0200

    added new migration with role db updates

---
+++ EF_Example/Migrations/20230318162118_InitialMigration.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Example.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Book_Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Catergory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(45)", maxLength: 45, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserData",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_id = table.Column<long>(type: "bigint", nullable: false),
+                    Book_Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserData", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLogin",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Login_date = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLogin", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User_Role",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User_Role", x => x.Id);
+                    table.ForeignKey(
+                        name: "RoleUserRoles",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "UserUserRolesFK",
+                        column: x => x.RoleId,
+                        principalTable: "UserLogin",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_id",
+                table: "Books",
+                column: "id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Id",
+                table: "Roles",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Role_Id",
+                table: "User_Role",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "RoleUserRoles_idx",
+                table: "User_Role",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleUserRolesFK_idx",
+                table: "User_Role",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserData_id",
+                table: "UserData",
+                column: "id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLogin_id",
+                table: "UserLogin",
+                column: "id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "User_Role");
+
+            migrationBuilder.DropTable(
+                name: "UserData");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "UserLogin");
+        }
+    }
+}
