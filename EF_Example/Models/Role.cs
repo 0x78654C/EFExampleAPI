0d08e94f19b88e368536cea4ec683bb4ef4a7ae7
x_Coding <xcoding.dev@gmail.com>
2023-03-19 08:59:27 +0200

    added role model

---
+++ EF_Example/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace EF_Example.Models
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public virtual ICollection<UseRole> UserRoles { get; set; } = new List<UseRole>();
+    }
+}
