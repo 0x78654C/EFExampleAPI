7b536ce834153eb5fd9b8e09cf77ddf17f33b6fd
x_Coding <xcoding.dev@gmail.com>
2023-03-19 22:19:33 +0200

    addded ISBN and fixed typo on category

--- EF_Example/Models/Books.cs
+++ EF_Example/Models/Books.cs
@@ -3,9 +3,10 @@
     public class Books
     {
         public long id { get; set; }
+        public string? ISBN { get; set; }
         public string? Book_Name { get; set; }
         public string? Author { get; set; }
-        public string?  Catergory { get; set; }
+        public string?  Category { get; set; }
 
         public int Amount { get; set; }
         public int Price { get; set; }
