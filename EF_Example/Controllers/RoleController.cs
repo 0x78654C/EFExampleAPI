30a7b6313638979bc9042141a0c83d94d1459892
x_Coding <xcoding.dev@gmail.com>
2023-03-19 08:57:29 +0200

    added controller for role class

---
+++ EF_Example/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using EF_Example.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Example.Controllers
+{
+   // [Authorize]
+    [Route("api/roles")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly ExampleDbContext _context;
+
+        public RoleController(ExampleDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// List all books in store.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "ListRoles")]
+        public async Task<IEnumerable<Role>> ListBooks() => await _context.Roles.ToListAsync();
+
+    }
+}
