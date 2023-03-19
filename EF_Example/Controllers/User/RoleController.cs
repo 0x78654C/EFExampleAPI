16c1b6dc2ad27cac8bb9dcb6ee5e10c6c1aed9df
x_Coding <xcoding.dev@gmail.com>
2023-03-19 22:17:06 +0200

    moved to user directory

---
+++ EF_Example/Controllers/User/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using EF_Example.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Example.Controllers.User
+{
+    [Authorize]
+    [Route("api/roles")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly ExampleDbContext _context;
+
+        public RoleController(ExampleDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// List all books in store.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "ListRoles")]
+        public async Task<IEnumerable<UseRole>> ListBooks() => await _context.UseRoles.ToListAsync();
+    }
+}
