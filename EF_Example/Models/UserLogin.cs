e8969126c983103efce82898709d5341ac8c5bec
x_Coding <xcoding.dev@gmail.com>
2023-03-20 21:15:07 +0200

    foreign key created on UserRole

--- EF_Example/Models/UserLogin.cs
+++ EF_Example/Models/UserLogin.cs
@@ -1,11 +1,14 @@
-﻿namespace EF_Example.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Example.Models
 {
     public class UserLogin
     {
         public long id { get; set; }
         public string? User_name { get; set; }
         public string? Password { get; set; }
-        public int? User_Role { get; set; }
+        [ForeignKey("UserRole")]
+        public UseRole? User_Role { get; set; }
         public DateTime Login_date { get; set; } = DateTime.UtcNow;
 
     }
