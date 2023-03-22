561abda0f2670876e72a1f35cd23b72ef1b3f8fa
x_Coding <xcoding.dev@gmail.com>
2023-03-22 21:59:09 +0200

    removed foreing key temporary

--- EF_Example/Migrations/20230322183921_RemoveForeignKey.Designer.cs
+++ EF_Example/Migrations/20230322183921_RemoveForeignKey.Designer.cs
@@ -4,6 +4,7 @@ using EF_Example;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF_Example.Migrations
 {
     [DbContext(typeof(ExampleDbContext))]
-    partial class ExampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230322183921_RemoveForeignKey")]
+    partial class RemoveForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EF_Example/Migrations/20230322183921_RemoveForeignKey.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Example.Migrations
+{
+    public partial class RemoveForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLogin_User_Role_UserRole",
+                table: "UserLogin");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserLogin_UserRole",
+                table: "UserLogin");
+
+            migrationBuilder.DropColumn(
+                name: "UserRole",
+                table: "UserLogin");
+
+            migrationBuilder.AddColumn<int>(
+                name: "User_Role",
+                table: "UserLogin",
+                type: "int",
+                maxLength: 3,
+                nullable: true,
+                defaultValue: 3);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User_Role",
+                table: "UserLogin");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserRole",
+                table: "UserLogin",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLogin_UserRole",
+                table: "UserLogin",
+                column: "UserRole");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLogin_User_Role_UserRole",
+                table: "UserLogin",
+                column: "UserRole",
+                principalTable: "User_Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
