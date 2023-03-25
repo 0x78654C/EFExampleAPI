60c4e1f5ab18ac1ab8ccb0a1eee3de655bdff7fa
x_Coding <xcoding.dev@gmail.com>
2023-03-25 22:49:10 +0200

    added role id global variable

--- FrontEndForm/Utils/Global.cs
+++ FrontEndForm/Utils/Global.cs
@@ -5,6 +5,7 @@ namespace FrontEndForm.Utils
     public class Global
     {
         public const string apiUrl = "https://localhost:7142";
-        public static SecureString jwtKey { get; set; }
+        public static string? roleId { get; set; }
+        public static SecureString? jwtKey { get; set; }
     }
 }
